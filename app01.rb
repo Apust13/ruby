3d0eddd31a49b5550b5ac95b86b188130f511dac
Alexander Nikitin <alexander.nikitin@corp.solbeg.com>
2019-02-18 12:31:46 +0300

    Initial commit

---
+++ app01.rb
@@ -0,0 +1,5 @@
+puts "One "," Two"," Three"
+puts "One "" Two"" Three"
+
+printf "One \n"," Two"," Three"
+printf "One "" Two"" Three"
---
+++ app03_types.rb
@@ -0,0 +1,7 @@
+puts "2".class
+puts 2.class
+puts "#{2.class}"
+puts "#{2.0.class}"
+puts "#{2e-2.class}"
+puts "#{1101.class}"
+puts "#{11110111010101010.class}"
